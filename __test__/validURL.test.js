7f937840c48da5c1045c4e9bc8bf6e9feb5b21f2
Rafah <RafahFilfilan@gmail.com>
2020-12-08 14:56:17 +0300

    Update validURL.test.js

--- __test__/validURL.test.js
+++ __test__/validURL.test.js
@@ -6,9 +6,10 @@ import { validURL } from "../src/client/js/formHandler"
 describe("Testing the validity of the text/url", () => {
     // The test() function has two arguments - a string description, and an actual test as a callback function.  
     test("Testing the validURL function", () => {
-           // Define the input for the function, if any, in the form of variables/array
-           // Define the expected output, if any, in the form of variables/array
-           // The expect() function, in combination with a Jest matcher, is used to check if the function produces the expected output
-           // The general syntax is `expect(myFunction(arg1, arg2, ...)).toEqual(expectedValue);`, where `toEqual()` is a matcher
-           expect(validURL).toBeDefined();
-})});
+		// Define the input for the function, if any, in the form of variables/array
+		// Define the expected output, if any, in the form of variables/array
+		// The expect() function, in combination with a Jest matcher, is used to check if the function produces the expected output
+		// The general syntax is `expect(myFunction(arg1, arg2, ...)).toEqual(expectedValue);`, where `toEqual()` is a matcher
+		expect(validURL).toBe();
+	})
+});
